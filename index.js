e2af42fb48adb6b7a70ee9bd2ba786d617f3b7a4
Pawan Sankalpa <Pawansankalpanew123@gmail.com>
2025-05-28 16:51:07 +0530

    Connect Nhost dabase through connectionString

--- index.js
+++ index.js
@@ -24,12 +24,8 @@ let posts = [];
 
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  ssl: true,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 db.connect()
